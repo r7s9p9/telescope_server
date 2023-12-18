8416b569c96e2db9a9f57fac6bd03a40a1909b9b
r7s9p9 <118193598+r7s9p9@users.noreply.github.com>
2023-12-18 21:11:21 +0300

    launching app via async

--- src/app.ts
+++ src/app.ts
@@ -1,81 +1,62 @@
-/**
- * @type {import('fastify').FastifyInstance} Instance of Fastify
- */
-
-import Fastify from 'fastify';
-import { authRoute } from './modules/auth/auth.route'
-import fastifyEnv from '@fastify/env';
-import fjwt, { JWT } from "@fastify/jwt";
+import Fastify, { FastifyInstance, FastifyPluginCallback } from 'fastify'
 import z from 'zod';
+import fp from 'fastify-plugin'
+import dotenv from 'dotenv'
+import { authRoute } from './modules/auth/auth.route'
+import fjwt, { JWT } from "@fastify/jwt";
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const config = dotenv.config({ path: isProduction ? '.env' : '.env.local' })
 
 const envVars = z.object({
-	DB_HOST: z.string(),
-	DB_USER: z.string(),
-	DB_PORT: z.string(),
-	DB_NAME: z.string(),
-	DB_PASS: z.string(),
-	JWT_SECRET: z.string()
-})
-
-//envVars.parse(process.env);
-
-const schema = {
-	type: 'object',
-	properties: {
-		envVars
-	}
-}
-
-const options = {
-	schema: schema,
-	dotenv: true,
-}
-
-declare module "fastify" {
-	interface FastifyRequest {
-		jwt: JWT;
-	}
-	export interface FastifyInstance {
-		authenticate: any;
-		config: {
-			JWT_SECRET: string
-		}
-	}
-}
-
-declare module "@fastify/jwt" {
-	interface FastifyJWT {
-		user: {
-			id: number;
-			email: string;
-			username: string;
-		};
-	}
-}
-
-
-
-const fastify = Fastify({
-	logger: true
-})
-
-fastify.register(fastifyEnv, options)
-	.ready((err: any) => {
-		if (err) console.error(err)
-
-		console.log(fastify.config) // or fastify[options.confKey]
-		// output: { PORT: 3000 }
-	})
-
-fastify.register(fjwt, {
-	secret: fastify.config.JWT_SECRET, // change this
+    APP_PORT: z.string(),
+    DB_HOST: z.string(),
+    DB_USER: z.string(),
+    DB_PORT: z.string(),
+    DB_NAME: z.string(),
+    DB_PASS: z.string(),
+    JWT_SECRET: z.string()
 });
 
-fastify.register(authRoute, { prefix: "api/auth" })
+const environment = envVars.parse(config.parsed);
 
-fastify.listen({ port: 3000 }, function (err, address) {
-	if (err) {
-		fastify.log.error(err)
-		process.exit(1)
-	}
+const fastifyEnv = fp(async (fastify: FastifyInstance, done: (err?: any) => void): Promise<void> => {
+    fastify.decorate('config', environment)
+}, {
+    name: 'custom-env',
+    fastify: '4.x',
 })
+
+declare module 'fastify' {
+    interface FastifyInstance {
+        config: z.infer<typeof envVars>
+    }
+}
+
+const app = async () => {
+    const fastify = Fastify({
+        logger: true
+    })
+
+    await fastify.register(fastifyEnv);
+
+    fastify.register(fjwt, {
+        secret: fastify.config.JWT_SECRET, // change this
+    });
+
+    fastify.register(authRoute, { prefix: "api/auth" })
+
+    await fastify.ready()
+
+    fastify.listen({ port: parseInt(fastify.config.APP_PORT) }, function (err, address) {
+        if (err) {
+            fastify.log.error(err)
+            process.exit(1)
+        }
+    })
+
+    return fastify;
+};
+
+app();
